fca36c867690349c27bfad1fedf9b77510e84aa1
YROOM <30819186+YROOM@users.noreply.github.com>
2022-04-17 10:30:12 +0800

    Yroombranch1 (#1)
    * just commit some change
    
    * 这是一次提交
    
    * 第二次提交
    
    * 修改：     src/main/java/com/example/thread/TestGit.java

---
+++ src/main/java/com/example/thread/TestGit.java
@@ -0,0 +1,16 @@
+package com.example.thread;
+
+/**
+ * @author YROOM
+ * @date 2022/4/16 下午6:29
+ * @description this class is used to
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    System.out.println("sdas");
+    }
+}
+
+
--- src/main/java/com/example/thread/study/StudyGit.java
+++
@@ -1,9 +0,0 @@
-package com.example.thread.study;
-
-public class StudyGit {
-  /** 这是一个学习git的例子 */
-  public static void main(String[] args) {
-    //
-    System.out.println("yroom git init git");
-  }
-}
